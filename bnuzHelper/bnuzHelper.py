72d19f8877a54aad1935e7cd41102109ec839d4d
Li Mingyu <202311079349@mail.bnu.edu.cn>
2025-12-07 23:24:36 +0800

    Update bnuzHelper.py

--- bnuzHelper/bnuzHelper.py
+++ bnuzHelper/bnuzHelper.py
@@ -17,8 +17,10 @@ from langchain_core.prompts import PromptTemplate
 import streamlit as st
 
 def get_api_key():
-
-    # 读系统文件
+    # 优先读 Secrets (云端)
+    # if "DEEPSEEK_API_KEY" in st.secrets:
+    #     return st.secrets["DEEPSEEK_API_KEY"]
+    # 其次读系统文件
     if(os.environ.get("DEEPSEEK_API_KEY")!=None):
         return os.environ.get("DEEPSEEK_API_KEY")
     #读本地文件
